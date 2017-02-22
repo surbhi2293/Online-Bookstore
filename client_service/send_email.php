ad51c2cc89bc23b1756545b91fabf1956fe5684f
Surbhi Mathur <mathur.surbhi22@gmail.com>
2017-02-21 20:06:29 -0600

    Update send_email.php

--- client_service/send_email.php
+++ client_service/send_email.php
@@ -5,7 +5,7 @@
 $emailTo = $_REQUEST["to"];
 print_r($_REQUEST);
 
-		$emailTo = "srikanth90.krish@gmail.com";
+		$emailTo = "surbhimathur@gmail.com";
 		//$emailTo = $user->getEmail();
 		
 $command = "powershell C:\\wamp\\www\\client_service\\email.ps1 ".$emailTo;
